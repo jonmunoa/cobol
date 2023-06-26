6356f25448a6e20d09c4607670d2ccc44704f363
Jon <jmunoa91@gmail.com>
2023-06-26 10:48:07 +0200

    Create test.cob

---
+++ test.cob
@@ -0,0 +1,23 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. SUMA.
+AUTHOR. TuNombre.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01 NUM1 PIC 9(3).
+01 NUM2 PIC 9(3).
+01 RESULTADO PIC 9(4).
+
+PROCEDURE DIVISION.
+MAIN-LOGIC.
+    DISPLAY "Ingrese el primer número: ".
+    ACCEPT NUM1.
+
+    DISPLAY "Ingrese el segundo número: ".
+    ACCEPT NUM2.
+
+    COMPUTE RESULTADO = NUM1 + NUM2.
+
+    DISPLAY "La suma de los números es: ", RESULTADO.
+
+    STOP RUN.
